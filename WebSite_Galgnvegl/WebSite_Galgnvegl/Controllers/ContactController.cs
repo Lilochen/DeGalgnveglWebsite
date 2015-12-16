413b367fec9f0bb29f1b8aa7aa7c6460eb1649fe
Lilochen <lilo.ich@web.de>
2015-12-16 09:11:50 +0100

    Contact_E-Mail senden (noch ohne Captcha)
    Funtzt

--- WebSite_Galgnvegl/WebSite_Galgnvegl/Controllers/ContactController.cs
+++ WebSite_Galgnvegl/WebSite_Galgnvegl/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace WebSite_Galgnvegl.Controllers
 {
@@ -20,6 +21,45 @@ namespace WebSite_Galgnvegl.Controllers
         {
             ViewBag.Message = "Kontaktiere uns:";
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEMail(string Mail_Subject, string Mail_Message, string Mail_Name, string Mail_Tel, string Mail_To)
+        {
+            MailMessage mail = new MailMessage();
+
+
+            mail.From = new MailAddress(Mail_To);
+            mail.To.Add("DeGalgnvegl@gmail.com");
+            mail.Subject = Mail_Subject;
+            if (Mail_Tel == "")
+            {
+                mail.Body = "E-Mail-Adresse: " + Mail_To + "\n" + "Name: " + Mail_Name + "\n" + "Telefon: Nicht angegeben" + "\n" + "Nachricht:\n" + Mail_Message;
+            }
+            else
+            {
+                mail.Body = "E-Mail-Adresse: " + Mail_To + "\n" + "Name: " + Mail_Name + "\n" + "Telefon: " + Mail_Tel + "\n" + "Nachricht:\n" + Mail_Message;
+            }
+
+            SmtpClient Smtpclient = new SmtpClient("smtp.gmail.com", 587);
+
+            try
+            {
+                Smtpclient.Credentials = new System.Net.NetworkCredential("DeGalgnvegl@gmail.com", "Galgnvogl");        //Anmeldung am SMTP-Server
+                Smtpclient.EnableSsl = true;                            //SSL Verschlüsselung erlauben                
+                Smtpclient.Send(mail);                                  //Senden der E-Mail
+
+                WebSite_Galgnvegl.Models.Result result = new Models.Result(true);
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                WebSite_Galgnvegl.Models.Result result = new Models.Result(false);
+                return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+
         }
 
     }
---
+++ WebSite_Galgnvegl/WebSite_Galgnvegl/Models/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite_Galgnvegl.Models
+{
+    public class Result
+    {
+        public bool result { get; set; }
+
+        public Result(bool _result)
+        {
+            result = _result;
+        }
+    }
+}
