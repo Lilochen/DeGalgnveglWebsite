0686d4b3cd1ef6bd1d923a4cae42c8a8e332bf9b
Lilochen <lilo.ich@web.de>
2015-12-18 10:58:17 +0100

    Captcha funzt :-D Endlich
    Kontaktformular ist fertig :-D

--- WebSite_Galgnvegl/WebSite_Galgnvegl/Controllers/ContactController.cs
+++ WebSite_Galgnvegl/WebSite_Galgnvegl/Controllers/ContactController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
 using BotDetect.Web.UI.Mvc;
+//using WebSite_Galgnvegl.Models.SharedModel;
 
 namespace WebSite_Galgnvegl.Controllers
 {
@@ -13,7 +14,8 @@ namespace WebSite_Galgnvegl.Controllers
     {
         //
         // GET: /Contact/
-
+        Models.SharedModel model = new Models.SharedModel();
+   
         public ActionResult Index()
         {
             return View();
@@ -21,55 +23,66 @@ namespace WebSite_Galgnvegl.Controllers
 
         public ActionResult ContactIndex()
         {
+           
             ViewBag.Message = "Kontaktiere uns:";
-            return View();
+            return View(model);
         }
 
         [HttpPost]
         [AllowAnonymous]
-        [CaptchaValidation("CaptchaCode", "SampleCaptcha", "Incorrect CAPTCHA code!")]
-        public ActionResult SendEMail(string Mail_Subject, string Mail_Message, string Mail_Name, string Mail_Tel, string Mail_To)
+        //[CaptchaValidation("CaptchaCode", "SampleCaptcha", "Incorrect CAPTCHA code!")]
+        public ActionResult SendEMail(string Mail_Subject, string Mail_Message, string Mail_Name, string Mail_Tel, string Mail_To, string CaptchaCode, string vcid)
         {
+            
             string errormessage;
             List<string> validation = new List<string>();
+            //bool isHuman = sampleCaptcha.Validate(CaptchaCode)
+            model.isHuman = model.myCaptcha.Validate(CaptchaCode, vcid);
 
-            if (String.IsNullOrWhiteSpace(Mail_Subject))
+            if (!model.isHuman)
             {
-                validation.Add("EmptySubject");
-            }
-            if (String.IsNullOrWhiteSpace(Mail_Message))
-            {
-                validation.Add("EmptyMessage");
-            }
-            if (String.IsNullOrWhiteSpace(Mail_Name))
-            {
-                validation.Add("EmptyName");
-            }
-            if (String.IsNullOrWhiteSpace(Mail_To))
-            {
-                validation.Add("EmptyMailAdress");
-            }
-            else
-            {   //MailValidation
-                bool IsValid = Regex.IsMatch(Mail_To, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
-                if (!IsValid)
-                {
-                    validation.Add("WrongMailAdress");
-                }
-            }
-
-            MailMessage mail = new MailMessage();
-
-            errormessage = string.Join(",", validation);
-
-            //CaptchaValidation
-            if (ModelState.IsValid)
-            {
-                WebSite_Galgnvegl.Models.Result CaptchaError = new Models.Result(false, "Bitte fülle das Captcha aus.");
+                WebSite_Galgnvegl.Models.Result CaptchaError = new Models.Result(false, "Bot");
                 return Json(CaptchaError, JsonRequestBehavior.AllowGet);
             }
             else
             {
+                if (String.IsNullOrWhiteSpace(Mail_Subject))
+                {
+                    validation.Add("EmptySubject");
+                }
+                if (String.IsNullOrWhiteSpace(Mail_Message))
+                {
+                    validation.Add("EmptyMessage");
+                }
+                if (String.IsNullOrWhiteSpace(Mail_Name))
+                {
+                    validation.Add("EmptyName");
+                }
+                if (String.IsNullOrWhiteSpace(Mail_To))
+                {
+                    validation.Add("EmptyMailAdress");
+                }
+                else
+                {   //MailValidation
+                    bool IsValid = Regex.IsMatch(Mail_To, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
+                    if (!IsValid)
+                    {
+                        validation.Add("WrongMailAdress");
+                    }
+                }
+
+                MailMessage mail = new MailMessage();
+
+                errormessage = string.Join(",", validation);
+
+                //CaptchaValidation
+                //if (ModelState.IsValid)
+                //{
+                //    WebSite_Galgnvegl.Models.Result CaptchaError = new Models.Result(false, "Bitte fülle das Captcha aus.");
+                //    return Json(CaptchaError, JsonRequestBehavior.AllowGet);
+                //}
+                //else
+                //{
                 if (!string.IsNullOrWhiteSpace(errormessage))
                 {
                     WebSite_Galgnvegl.Models.Result result = new Models.Result(false, errormessage);
--- WebSite_Galgnvegl/WebSite_Galgnvegl/Models/SharedModel.cs
+++ WebSite_Galgnvegl/WebSite_Galgnvegl/Models/SharedModel.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BotDetect.Web.UI.Mvc;
 
 namespace WebSite_Galgnvegl.Models
 {
     public class SharedModel
     {
         //var selectedData = db.Query("SELECT * FROM Markttermine1");
-        public WebMatrix.Data.Database db = WebMatrix.Data.Database.Open("Connect_dbMarkt");
+        //public WebMatrix.Data.Database db = WebMatrix.Data.Database.Open("Connect_dbMarkt");
         public string selectedData =  "SELECT 'Datum_von', 'Datum_bis', 'Marktname', 'Ort' FROM 'tbMarkttermine";
 
         public DateTime Date_from { get; set; }
@@ -16,10 +17,14 @@ namespace WebSite_Galgnvegl.Models
         public string Marketname { get; set; }
         public string Locationname { get; set; }
         public string URL { get; set; }
+        
+        public bool isHuman { get; set; }
+        public MvcCaptcha myCaptcha;
        
         public SharedModel()
         {
-            var model = HttpContext.Current.Session["SharedModel"];
+            myCaptcha = new MvcCaptcha("SampleCaptcha");
+            myCaptcha.UserInputClientID = "CapchaCode";
         }        
         
     }
